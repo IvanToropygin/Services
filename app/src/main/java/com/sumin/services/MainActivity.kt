0a30ce339dae3d4d66cfdad037caf489bfdeffc8
Ivan Toropygin <itori86@mail.ru>
2025-01-10 17:17:59 +0300

    Initial commit

---
+++ app/src/main/java/com/sumin/services/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.sumin.services
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.sumin.services.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
